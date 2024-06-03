23bb527c5ea637a48b032a2cb542afef22883098
JamesTheMaker <jamesthemaker2005@gmail.com>
2024-06-03 09:31:19 -0400

    Begin new hazard rework

--- stats/effects/electricblock/electricblock.lua
+++
@@ -1,9 +0,0 @@
-function init()
-    effect.addStatModifierGroup({{stat = "electricStatusImmunity", amount = 1}})
- 
-    script.setUpdateDelta(0)
-end
- 
-function update(dt) end
- 
-function uninit() end
